43a5123160270064fc253fb01cce53b45ed8eee9
Edward Schults <edward.schults@gmail.com>
2019-09-19 20:24:38 +0200

    pass locals

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,16 +3,23 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render partial: "form", status: :unprocessable_entity
-    end
+    @review.save
+    render partial: "reviews/reviews",
+           locals: { restaurant: @restaurant, review: review },
+           status: status
   end
 
   private
 
   def review_params
     params.require(:review).permit(:content)
+  end
+
+  def review
+    @review.valid? ? Review.new : @review
+  end
+
+  def status
+    @review.valid? ? :ok : :unprocessable_entity
   end
 end
